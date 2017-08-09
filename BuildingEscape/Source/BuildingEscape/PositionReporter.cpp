6469a034140b621f979b14d10203e9c2eeb6addc
DESKTOP-B74EV12\Mark <mark.joseph.daly@gmail.com>
2017-08-09 12:07:29 +0100

    GetPosition

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -18,8 +19,9 @@ UPositionReporter::UPositionReporter()
 void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on chair!"));
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectLocation = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"),*ObjectName,*ObjectLocation);
 	
 }
 
